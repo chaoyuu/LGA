a72005fdb2928bd24a11583f45b1f5dc7dc97f53
chaoyuu <85202801+chaoyuu@users.noreply.github.com>
2021-11-01 22:04:40 +1100

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,29 +1,7 @@
-<<<<<<< HEAD
-<?php 
-
-$sName = "localhost";
-$uName = "root";
-$pass = "";
-$db_name = "login";
-
-try {
-    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
-                    $uName, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-  echo "Connection failed : ". $e->getMessage();
-=======
 <?php
 
-$sname= "localhost";
-$unmae= "root";
-$password = "";
-
-$db_name = "user_db";
-
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$conn = mysqli_connect("localhost", "root", "", "login");
 
 if (!$conn) {
-	echo "Connection failed!";
->>>>>>> 6492c4f9982350bc4dc7a9406a9630b7d50bd2b1
+    echo "Connection Failed";
 }
