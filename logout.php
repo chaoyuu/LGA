6b5ed15892170159d9df8b9db4963869ac9e2c81
chaoyuu <85202801+chaoyuu@users.noreply.github.com>
2021-11-01 22:10:09 +1100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php 
 session_start();
 
 session_unset();
 session_destroy();
 
-=======
-<?php 
-session_start();
-
-session_unset();
-session_destroy();
-
->>>>>>> 6492c4f9982350bc4dc7a9406a9630b7d50bd2b1
 header("Location: index.php");
