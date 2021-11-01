df922a6c82dd09eedc1d4d7ee27fc4e3e3a87a91
chaoyuu <85202801+chaoyuu@users.noreply.github.com>
2021-11-01 22:07:52 +1100

    Update home.php

--- home.php
+++ home.php
@@ -1,128 +1,20 @@
-<<<<<<< HEAD
 <?php
     session_start();
-    if (isset($_SESSION['SESSION_EMAIL'])) {
+    if (!isset($_SESSION['SESSION_EMAIL'])) {
         header("Location: index.php");
         die();
     }
 
     include 'config.php';
-    $msg = "";
 
-    if (isset($_GET['verification'])) {
-        if (mysqli_num_rows(mysqli_query($conn, "SELECT * FROM users WHERE code='{$_GET['verification']}'")) > 0) {
-            $query = mysqli_query($conn, "UPDATE users SET code='' WHERE code='{$_GET['verification']}'");
-            
-            if ($query) {
-                $msg = "<div class='alert alert-success'>Account verification has been successfully completed.</div>";
-            }
-        } else {
-            header("Location: index.php");
-        }
-    }
+    $query = mysqli_query($conn, "SELECT * FROM users WHERE email='{$_SESSION['SESSION_EMAIL']}'");
 
-    if (isset($_POST['submit'])) {
-        $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $password = mysqli_real_escape_string($conn, md5($_POST['password']));
+    if (mysqli_num_rows($query) > 0) {
+        $row = mysqli_fetch_assoc($query);
 
-        $sql = "SELECT * FROM users WHERE email='{$email}' AND password='{$password}'";
-        $result = mysqli_query($conn, $sql);
-
-        if (mysqli_num_rows($result) === 1) {
-            $row = mysqli_fetch_assoc($result);
-
-            if (empty($row['code'])) {
-                $_SESSION['SESSION_EMAIL'] = $email;
-                header("Location: welcome.php");
-            } else {
-                $msg = "<div class='alert alert-info'>First verify your account and try again.</div>";
-            }
-        } else {
-            $msg = "<div class='alert alert-danger'>Email or password do not match.</div>";
-        }
+        echo "Welcome " . $row['name'] . " <a href='logout.php'>Logout</a>";
     }
 ?>
-
-<!DOCTYPE html>
-<html lang="zxx">
-
-<head>
-    <title>Public  schools address to LGA Mapping</title>
-    <!-- Meta tag Keywords -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="UTF-8" />
-    <meta name="keywords"
-        content="Login Form" />
-    <!-- //Meta tag Keywords -->
-
-    <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
-
-    <!--/Style-CSS -->
-    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
-    <!--//Style-CSS -->
-
-    <script src="https://kit.fontawesome.com/af562a2a63.js" crossorigin="anonymous"></script>
-
-</head>
-
-<body>
-
-    <!-- form section start -->
-    <section class="w3l-mockup-form">
-        <div class="container">
-            <!-- /form -->
-            <div class="workinghny-form-grid">
-                <div class="main-mockup">
-                    <div class="alert-close">
-                        <span class="fa fa-close"></span>
-                    </div>
-                    <div class="w3l_form align-self">
-                        <div class="left_grid_info">
-                            <img src="images/image.svg" alt="">
-                        </div>
-                    </div>
-                    <div class="content-wthree">
-                        <h2>Login Now</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                        <?php echo $msg; ?>
-                        <form action="" method="post">
-                            <input type="email" class="email" name="email" placeholder="Enter Your Email" required>
-                            <input type="password" class="password" name="password" placeholder="Enter Your Password" style="margin-bottom: 2px;" required>
-                            <p><a href="forgot-password.php" style="margin-bottom: 15px; display: block; text-align: right;">Forgot Password?</a></p>
-                            <button name="submit" name="submit" class="btn" type="submit">Login</button>
-                        </form>
-                        <div class="social-icons">
-                            <p>Create Account! <a href="register.php">Register</a>.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- //form -->
-        </div>
-    </section>
-    <!-- //form section start -->
-
-    <script src="js/jquery.min.js"></script>
-    <script>
-        $(document).ready(function (c) {
-            $('.alert-close').on('click', function (c) {
-                $('.main-mockup').fadeOut('slow', function (c) {
-                    $('.main-mockup').remove();
-                });
-            });
-        });
-    </script>
-
-</body>
-
-</html>
-=======
-<?php 
-session_start();
-
-if (isset($_SESSION['Id']) && isset($_SESSION['Username'])) {
-
- ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -138,8 +30,7 @@ if (isset($_SESSION['Id']) && isset($_SESSION['Username'])) {
       integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body>
-     <h1>Hello, <?php echo $_SESSION['Name']; ?></h1>
-     <a href="Logout.php">Logout</a>
+    
 <div class="container">
      <nav class="navbar navbar-expand-lg navbar-light bg-light">
      <div class="container-fluid">
@@ -189,11 +80,3 @@ if (isset($_SESSION['Id']) && isset($_SESSION['Username'])) {
 </nav>
 </body>
 </html>
-
-<?php 
-}else{
-     header("Location: index.php");
-     exit();
-}
- ?>
->>>>>>> 6492c4f9982350bc4dc7a9406a9630b7d50bd2b1
